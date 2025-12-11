99705b5d9412abfa92ba3b9669f2dbb12f08188d
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-12-11 17:48:40 -0300

    All starts here!

---
+++ backend/microservices/tower/src/main/java/com/chocolatada/tower/TowerApplication.java
@@ -0,0 +1,13 @@
+package com.chocolatada.tower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TowerApplication.class, args);
+	}
+
+}
