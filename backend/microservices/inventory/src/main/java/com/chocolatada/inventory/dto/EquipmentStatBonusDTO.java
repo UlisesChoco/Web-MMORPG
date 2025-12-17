b29622d09ebfb72c52ebafd784976363301ced28
chocolatada21 <ulisesjustosaucedo@gmail.com>
2025-12-17 18:17:24 -0300

    [WM-32] Implement Item repository and service

---
+++ backend/microservices/inventory/src/main/java/com/chocolatada/inventory/dto/EquipmentStatBonusDTO.java
@@ -0,0 +1,21 @@
+package com.chocolatada.inventory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EquipmentStatBonusDTO {
+    private Integer atk;
+    private Integer def;
+    private Integer stamina;
+    private Integer accuracy;
+    private Integer evasion;
+    private Float critRate;
+    private Float critDamage;
+}
+
