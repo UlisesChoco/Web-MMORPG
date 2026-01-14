ea564a214113c49c78d29e73b92c89ffbac55b86
chocolatada21 <ulisesjustosaucedo@gmail.com>
2026-01-14 19:03:35 -0300

    [WM-67] Implement Map RPC methods

--- backend/microservices/world/src/main/java/com/chocolatada/world/mapper/MapMapper.java
+++ backend/microservices/world/src/main/java/com/chocolatada/world/mapper/MapMapper.java
@@ -4,6 +4,7 @@ import com.chocolatada.world.dto.EnemyDTO;
 import com.chocolatada.world.dto.MapDTO;
 import com.chocolatada.world.dto.MapEnemyDTO;
 import com.chocolatada.world.entity.EnemyEntity;
+import com.chocolatada.world.entity.EnemyType;
 import com.chocolatada.world.entity.MapEntity;
 
 import java.util.ArrayList;
@@ -73,6 +74,83 @@ public class MapMapper {
         for (EnemyEntity entity : entities)
             dtos.add(toEnemyDTO(entity));
         return dtos;
+    }
+
+    public static com.chocolatada.world.grpc.Map toGrpcMap(MapDTO dto) {
+        return com.chocolatada.world.grpc.Map.newBuilder()
+                .setId(dto.getId())
+                .setName(dto.getName())
+                .setDescription(dto.getDescription())
+                .setRangeLevel(dto.getRangeLevel())
+                .build();
+    }
+
+    public static List<com.chocolatada.world.grpc.Map> toGrpcMaps(List<MapDTO> dtos) {
+        List<com.chocolatada.world.grpc.Map> grpcMaps = new ArrayList<>();
+        for (MapDTO dto : dtos)
+            grpcMaps.add(toGrpcMap(dto));
+        return grpcMaps;
+    }
+
+    public static com.chocolatada.world.grpc.MapEnemy toGrpcMapEnemy(MapEnemyDTO dto) {
+        return com.chocolatada.world.grpc.MapEnemy.newBuilder()
+                .setId(dto.getId())
+                .setName(dto.getName())
+                .setDescription(dto.getDescription())
+                .setGold(dto.getGold())
+                .build();
+    }
+
+    public static List<com.chocolatada.world.grpc.MapEnemy> toGrpcMapEnemies(List<MapEnemyDTO> dtos) {
+        if (dtos == null)
+            return new ArrayList<>();
+
+        List<com.chocolatada.world.grpc.MapEnemy> grpcEnemies = new ArrayList<>();
+        for (MapEnemyDTO dto : dtos)
+            grpcEnemies.add(toGrpcMapEnemy(dto));
+        return grpcEnemies;
+    }
+
+    public static com.chocolatada.world.grpc.Enemy toGrpcEnemy(EnemyDTO dto) {
+        return com.chocolatada.world.grpc.Enemy.newBuilder()
+                .setId(dto.getId())
+                .setName(dto.getName())
+                .setDescription(dto.getDescription())
+                .setType(toGrpcEnemyType(dto.getType()))
+                .setLevel(dto.getLevel())
+                .setExperience(dto.getExperience())
+                .setGold(dto.getGold())
+                .setCritRate(dto.getCritRate())
+                .setCritDamage(dto.getCritDamage())
+                .setAtk(dto.getAtk())
+                .setDef(dto.getDef())
+                .setStamina(dto.getStamina())
+                .setAccuracy(dto.getAccuracy())
+                .setEvasion(dto.getEvasion())
+                .build();
+    }
+
+    public static List<com.chocolatada.world.grpc.Enemy> toGrpcEnemies(List<EnemyDTO> dtos) {
+        if (dtos == null)
+            return new ArrayList<>();
+
+        List<com.chocolatada.world.grpc.Enemy> grpcEnemies = new ArrayList<>();
+        for (EnemyDTO dto : dtos)
+            grpcEnemies.add(toGrpcEnemy(dto));
+        return grpcEnemies;
+    }
+
+    public static com.chocolatada.world.grpc.EnemyType toGrpcEnemyType(EnemyType type) {
+        if (type == null)
+            return com.chocolatada.world.grpc.EnemyType.ENEMY_TYPE_UNSPECIFIED;
+
+        return switch (type) {
+            case NORMAL -> com.chocolatada.world.grpc.EnemyType.NORMAL;
+            case ELITE -> com.chocolatada.world.grpc.EnemyType.ELITE;
+            case BOSS -> com.chocolatada.world.grpc.EnemyType.BOSS;
+            case MINIBOSS -> com.chocolatada.world.grpc.EnemyType.MINIBOSS;
+            case LEGENDARY -> com.chocolatada.world.grpc.EnemyType.LEGENDARY;
+        };
     }
 }
 
